df1572a58d91c8c718fc3a55d20091b3cbd6ff0c
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-17 11:23:48 +0300

    game half done

--- app.js
+++ app.js
@@ -6,38 +6,62 @@ function rand(min, max) {
 
 const GameBalls = document.querySelectorAll(".ball");
 const oneBall = document.querySelector(".ball");
-console.log(oneBall);
+const reset = document.querySelector(".reset");
+
 let numArray = [];
 let randNum;
+function placeRandNum() {
+  do {
+    randNum = rand(1, 25);
+    if (!numArray.includes(randNum)) {
+      numArray.push(randNum);
+    }
+  } while (numArray.length < 25);
+}
 
-do {
-  randNum = rand(1, 25);
-  if (!numArray.includes(randNum)) {
-    numArray.push(randNum);
-  }
-} while (numArray.length < 25);
+placeRandNum();
 
-let startX = 10;
-let startY = 10;
-GameBalls.forEach((el, i) => {
-  el.innerHTML = numArray[i];
-  if (startX > 530) {
-    startX = 10;
-    startY += 110;
-    count = 0;
-  }
-  GameBalls[i].style.left = startX + "px";
-  GameBalls[i].style.top = startY + "px";
-  startX += 130;
-});
+function GameSet() {
+  let startX = 10;
+  let startY = 10;
+  GameBalls.forEach((el, i) => {
+    el.innerHTML = numArray[i];
+    if (startX > 530) {
+      startX = 10;
+      startY += 110;
+      count = 0;
+    }
+    el.style.left = startX + "px";
+    el.style.top = startY + "px";
+    startX += 130;
+  });
+}
+GameSet();
 
-// function getBalls() {
-//   GameBalls.forEach((a, i) => {
-//     GameBalls[0].style.right = "0px";
-//     GameBalls[0].style.top = "0px";
-//   });
-// }
+function getBalls() {
+  let startX_Set = 1180;
+  let startY_Set = 10;
+  let count2 = 1;
+  GameBalls.forEach((a, i) => {
+    a.addEventListener("click", (e) => {
+      if (a.innerText == count2) {
+        a.style.left = startX_Set + "px";
+        a.style.top = startY_Set + "px";
+        count2++;
+        startX_Set = startX_Set - 130;
+        if (startX_Set < 660) {
+          startX_Set = 1180;
+          startY_Set += 110;
+        }
+      }
+    });
+  });
+}
 
-oneBall.addEventListener("click", (_) => {
-  oneBall.style.left = "1180px";
+getBalls();
+
+reset.addEventListener("click", (_) => {
+  placeRandNum();
+  GameSet();
+  getBalls();
 });
