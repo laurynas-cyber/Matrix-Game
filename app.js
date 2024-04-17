99f4c88f28d0aaf1fa930fa7210301f43ad75807
laurynas-cyber <58857487+laurynas-cyber@users.noreply.github.com>
2024-04-18 00:09:21 +0300

    css done

--- app.js
+++ app.js
@@ -9,17 +9,25 @@ const oneBall = document.querySelector(".ball");
 const reset = document.querySelector(".reset");
 const start = document.querySelector(".start");
 
-
+const Clock = document.querySelector(".clock");
+// Clock.classList.remove("clock");
+console.log(Clock);
+const Alert = document.querySelector(".alert-message");
 
 let numArray = [];
 let randNum;
+let ColorsArray = [];
+let randColors;
 
 function placeRandNum() {
   do {
+    randColors = `(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`;
+    ColorsArray.push("rgb" + randColors);
     if (numArray.length == 25) {
       numArray = [];
     }
     randNum = rand(1, 25);
+
     if (!numArray.includes(randNum)) {
       numArray.push(randNum);
     }
@@ -27,10 +35,7 @@ function placeRandNum() {
 }
 
 placeRandNum();
-console.log(numArray);
-placeRandNum();
-console.log(numArray);
-
+console.log(randColors, ColorsArray);
 function GameSet() {
   let startX = 10;
   let startY = 10;
@@ -43,6 +48,10 @@ function GameSet() {
     }
     el.style.left = startX + "px";
     el.style.top = startY + "px";
+    // el.style.backgroundColor = ColorsArray[i];
+    el.style.backgroundImage = `linear-gradient(90deg, ${
+      ColorsArray[rand(0, ColorsArray.length)]
+    }, ${ColorsArray[rand(0, ColorsArray.length)]})`;
     startX += 130;
   });
 }
@@ -70,13 +79,63 @@ function getBalls() {
 
 getBalls();
 
+//AI kodas
+// start.addEventListener("click", startCountdown);
+
+// function startCountdown() {
+//   // Set the countdown time in seconds
+//   var countdownTime = 10; // 60 seconds
+
+//   // Update the display immediately to show the starting time
+//   updateDisplay(countdownTime);
+
+//   // Start the countdown interval
+//   var countdownInterval = setInterval(function () {
+//     countdownTime--;
+//     updateDisplay(countdownTime);
+
+//     // If countdown reaches zero, clear the interval
+//     if (countdownTime <= 0) {
+//       clearInterval(countdownInterval);
+//       alert("Countdown complete!");
+//     }
+//   }, 1000); // Update every 1 second (1000 milliseconds)
+// }
+
+// function updateDisplay(time) {
+//   // Format seconds into minutes:seconds format
+//   var minutes = Math.floor(time / 60);
+//   var seconds = time % 60;
+
+//   // Display the formatted time in the countdownDisplay element
+//   document.getElementById("clock").innerText = minutes + "m " + seconds + "s";
+// }
+
+// mano kodas
+start.addEventListener("click", (e) => {
+  GameBalls.forEach((e) => (e.style.visibility = "visible"));
+  Clock.innerText = 10;
+  setInterval(ClockGo, 1000); //pasileidzia dvi funkcijos
+  // clearInterval(seconds);
+  // console.log(e.type, e);
+});
+
 reset.addEventListener("click", (_) => {
   placeRandNum();
   console.log(numArray);
   GameSet();
-  getBalls();
+  getBalls(); //pasileidzia dvi funkcijos
+  Clock.innerText = 10;
+  // setInterval(ClockGo, 1000);
 });
 
-start.addEventListener("click", (_) => {
-  GameBalls.forEach((e) => (e.style.visibility = "visible"));
-});
+function ClockGo() {
+  if (Clock.innerText != 0) {
+    Clock.innerText = parseInt(Clock.innerText) - 1;
+  } else if (Clock.innerText >= 0) {
+    Clock.innerText = 0;
+    return (Alert.innerText = "Your time ran out");
+  }
+}
+
+// let seconds = setInterval(ClockGo, 1000);
